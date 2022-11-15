89e58c75105848ac9329b5ad1f9dc428c926f8ce
Fab <fabiomiguelchenhuang@gmail.com>
2022-11-15 23:33:34 +0000

    added query 2 code

--- src/scripts/data_cleaning.py
+++ src/scripts/data_cleaning.py
@@ -6,7 +6,7 @@ df = pd.read_json('reviews.json', orient='records')
 
 df.drop(['artist_y', 'best_new_music', 'author_type', 'pub_weekday', 'pub_day', 'pub_month', 'pub_year', 'url'], inplace=True, axis=1)
 
-df = df.rename(columns={"pub_date": "review_publication_date", "year": "song_release_year", "label": "record_label", "content":"review_content", "artist_x": "artist"})
+df = df.rename(columns={"pub_date": "review_publication_date", "year": "song_release_year", "label": "record_label", "content":"review_content", "artist_x": "artist", "score": "rating_score"} )
 
 df['review_publication_date'] = pd.to_datetime(df['review_publication_date'], format='%Y-%m-%d')
 
